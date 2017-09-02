e862a0d7cbb27bc305d66fafe9def76820d350d6
Olga Filipova <chudaol@gmail.com>
2017-09-02 03:23:50 +0300

    almost finished

--- src/App.vue
+++ src/App.vue
@@ -6,13 +6,13 @@
     <workout-component></workout-component>
     <div class="slides">
       <section>
-        <h1>Hi, my name is Olga</h1>
+        <h2>Hi, my name is Olga</h2>
         <div>
           <div class="logos row align-items-center">
             <div class="col-6">
-              <img src="./assets/optiopay-logo.svg" alt="">
+              <img src="./assets/optiopay-logo.png" alt="">
             </div>
-            <div class="col-6">
+            <div class="col-6 text-right">
               <img src="./assets/edera_logo.png" alt="">
             </div>
           </div>
@@ -22,13 +22,24 @@
         </aside>
       </section>
       <section>
-        <h1>How I manage time and stay fit with Vue.js</h1>
+        <h2>How I manage time and stay fit with Vue.js</h2>
         <div>
           <tomato></tomato>
         </div>
       </section>
       <section>
-        <h1>How I wrote a book about Vue.js</h1>
+        <h2>Outline</h2>
+        <ul>
+          <li>How I wrote a book about Vue.js</li>
+          <li>What is ProFitOro</li>
+          <li>Vue, Vuex, Nuxt</li>
+          <li>Firebase - auth, datastorage, database, hosting</li>
+          <li>Jest - snapshot testing</li>
+          <li>CircleCI</li>
+        </ul>
+      </section>
+      <section>
+        <h2>How I wrote a book about Vue.js</h2>
       </section>
       <section>
         <h2>Learning Vue.js 2</h2>
@@ -40,7 +51,158 @@
       <section>
         <h2>How it all started</h2>
         <p>11/04/2016 11:30</p>
-        <img src="img/email1.png" alt="first email">
+        <img src="./assets/images/email1.png" alt="first email">
+      </section>
+      <section>
+        <h2>How it all started</h2>
+        <p>11/04/2016 12:30</p>
+        <img src="./assets/images/email2.png" alt="lol">
+      </section>
+      <section>
+        <h2>How it all started</h2>
+        <p>12/04/2016 09:30</p>
+        <img src="./assets/images/email3.png" alt="first email">
+      </section>
+      <section>
+        <h2>Family support</h2>
+        <img src="./assets/images/review.jpg" alt="Reviews">
+      </section>
+      <section>
+        <h2>What is the book about</h2>
+        <ul>
+          <li class="fragment">Two applications</li>
+          <li class="fragment">Developed from scratch</li>
+          <li class="fragment">Going through unit tests and e2e tests</li>
+          <li class="fragment">Deployed + CI</li>
+        </ul>
+      </section>
+      <section>
+        <h2>Problems</h2>
+        <img src="./assets/images/vue2.png" alt="">
+      </section>
+      <section>
+        <h2>Happiness</h2>
+        <img src="./assets/images/photowithbook.jpg" alt="">
+      </section>
+      <section>
+        <h2>Surprise</h2>
+        <img src="./assets/images/hackernews.jpg" alt="">
+      </section>
+      <section>
+        <h2>Still</h2>
+        <p>Writing books? Never ever again!</p>
+      </section>
+      <section>
+        <h2>All of a sudden</h2>
+        <img src="./assets/images/email4.png" alt="">
+      </section>
+      <section>
+        <h2>Let's do it!</h2>
+        <img src="./assets/images/profitoro_logo.svg" alt="">
+      </section>
+      <section>
+        <h2>What is ProFitOro?</h2>
+        <p><a href="https://profitorolife.com/">https://profitorolife.com/</a></p>
+        <img class="not-so-big" src="./assets/images/background.png" alt="background">
+      </section>
+      <section>
+        <h2>
+          What is ProFitOro?
+        </h2>
+        <ul>
+          <li>Pomodoro timer</li>
+          <li>Workouts</li>
+        </ul>
+      </section>
+      <section>
+        <img src="./assets/images/workout.png" alt="">
+      </section>
+      <section>
+        <h2>Vue, Vuex, Nuxt</h2>
+        <p>Nuxt is awesome</p>
+        <div class="left-column fragment">
+          <h4>Vue router</h4>
+<pre><code data-trim>
+//components/Pomodoro.vue
+const Pomodoro = {
+  template: 'Timer'
+}
+//components/About.vue
+const About = {
+  template: 'This is ProFitOro'
+}
+//router.js
+const router = new VueRouter({
+  routes: [
+    { path: '/pomodoro', component: Pomodoro },
+    { path: '/about', component: About }
+  ]
+})
+          </code>
+        </pre>
+        </div>
+        <div class="right-column fragment">
+          <h4>Nuxt</h4>
+          <pre>
+            <code>
+//pages/pomodoro.vue
+const Pomodoro = {
+  template: 'Timer'
+}
+//pages/about.vue
+const About = {
+  template: 'This is ProFitOro'
+}
+            </code>
+          </pre>
+        </div>
+      </section>
+      <section>
+        <h2>Nuxt - middleware</h2>
+        <div class="left-column fragment">
+          <pre>
+          <code>
+//middleware/authenticated.js
+export default function ({ store, redirect }) {
+  if (!store.getters.isAuthenticated) {
+    return redirect('/')
+  }
+}
+          </code>
+        </pre>
+        </div>
+        <div class="right-column fragment">
+          <pre>
+            <code class="html" data-trim data-noescape>
+//pages/workouts.vue
+&lt;template&gt;
+  workouts
+&lt;/template&gt;
+&lt;script&gt;
+ export default {
+   middleware: 'authenticated',
+ }
+&lt;/script&gt;
+&lt;style&gt;
+  /*...*/
+&lt;/style&gt;
+            </code>
+          </pre>
+        </div>
+      </section>
+      <section>
+        <h2>Firebase</h2>
+      </section>
+      <section>
+        <h2>Jest</h2>
+      </section>
+      <section>
+        <h2>CircleCI</h2>
+      </section>
+      <section>
+        <h2>Conclusion</h2>
+        <p class="fragment">Do not hesitate</p>
+        <p class="fragment">Just do it <span class="big-text pink">&hearts;</span></p>
       </section>
     </div>
   </div>
@@ -71,11 +233,19 @@
         // default/cube/page/concave/zoom/linear/fade/none
         transition: 'default'
       })
+      hljs.initHighlightingOnLoad();
     }
   }
 </script>
 
 <style scoped lang="scss">
+  img.not-so-big {
+    max-height: 70vh!important;
+  }
+  .logos {
+    width: 80%;
+    margin: auto;
+  }
   .logos img {
     padding: 20px;
     background: transparent;
